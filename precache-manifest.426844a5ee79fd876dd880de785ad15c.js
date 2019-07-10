b4a30aa1b98c683526c2cd9d4acf28354417faeb
aginert <ana.ginert@gmail.com>
2019-07-10 12:23:39 +0200

    Updates

---
+++ precache-manifest.426844a5ee79fd876dd880de785ad15c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "72c6c24167be233bdc0f",
+    "url": "/badges/static/css/main.8da1fdd9.chunk.css"
+  },
+  {
+    "revision": "72c6c24167be233bdc0f",
+    "url": "/badges/static/js/main.72c6c241.chunk.js"
+  },
+  {
+    "revision": "21a534598d04da462866",
+    "url": "/badges/static/js/runtime~main.21a53459.js"
+  },
+  {
+    "revision": "184035bf3745301f6245",
+    "url": "/badges/static/css/2.d807360f.chunk.css"
+  },
+  {
+    "revision": "184035bf3745301f6245",
+    "url": "/badges/static/js/2.184035bf.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/badges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/badges/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/badges/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/badges/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "521fdb33b96d7707bd4a585562ec0941",
+    "url": "/badges/static/media/error.521fdb33.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/badges/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "b8d554c81be9966f6d08aa3dc5d000da",
+    "url": "/badges/index.html"
+  }
+];
